f4f3c999e1995f54ff5aaf170515e2d5dd34b9d3
dhanbahadur12 <115434712+dhanbahadur12@users.noreply.github.com>
2022-11-04 14:40:28 +0545

    Add files via upload

---
+++ create.js
@@ -0,0 +1,18 @@
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+      return (tasks.innerHTML += `
+      <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <p>${x.description}</p>
+    
+            <span class="options">
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+            </span>
+          </div>
+      `);
+    });
+  
+    resetForm();
+  };
