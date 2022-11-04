1cad771966d79d6a79bd2ca9526ba11bb2d664f8
dhanbahadur12 <115434712+dhanbahadur12@users.noreply.github.com>
2022-11-04 14:46:57 +0545

    Add files via upload

---
+++ delete.js
@@ -0,0 +1,8 @@
+
+let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id, 1);
+    localStorage.setItem("data", JSON.stringify(data));
+    console.log(data);
+    
+  };
